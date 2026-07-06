36ebeddc66a59a7fb913573932af3ac2a99b8589
Ayka11 <32898816+Ayka11@users.noreply.github.com>
2026-07-06 17:14:37 +0400

    Create JobCollector class for job data collection
    Implement JobCollector class to collect job data.

---
+++ agents/collector.py
@@ -0,0 +1,65 @@
+class JobCollector:
+
+    def collect(self):
+
+        """
+        Temporary data.
+
+        Later this module will connect to:
+
+        RemoteOK
+
+        Wellfound
+
+        We Work Remotely
+
+        Remotive
+
+        Company career pages
+        """
+
+        return [
+
+            {
+
+                "company": "Example Company",
+
+                "title": "Technical Writer",
+
+                "remote": True,
+
+                "hours": "Flexible",
+
+                "skills": [
+
+                    "python",
+
+                    "documentation",
+
+                    "git"
+
+                ]
+
+            },
+
+            {
+
+                "company": "Example Startup",
+
+                "title": "Research Assistant",
+
+                "remote": True,
+
+                "hours": "Part-time",
+
+                "skills": [
+
+                    "research",
+
+                    "writing"
+
+                ]
+
+            }
+
+        ]
